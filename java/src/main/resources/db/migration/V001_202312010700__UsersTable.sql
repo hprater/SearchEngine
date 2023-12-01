aa31765481e14f284af2b50e8c6e829838bc90aa
Brad Starkenberg <brad@starkenberg.net>
2023-12-01 07:33:10 -0600

    update port for local dev and add flyway

--- java/src/main/resources/db/migration/V001_202312010700__UsersTable.sql
+++ java/src/main/resources/db/migration/V001_202312010700__UsersTable.sql
@@ -1,8 +1,10 @@
-BEGIN TRANSACTION;
+CREATE TABLE users (
+	user_id SERIAL,
+	username varchar(50) NOT NULL UNIQUE,
+	password_hash varchar(200) NOT NULL,
+	role varchar(50) NOT NULL,
+	CONSTRAINT PK_user PRIMARY KEY (user_id)
+);
 
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
-
-
-COMMIT TRANSACTION;
-
